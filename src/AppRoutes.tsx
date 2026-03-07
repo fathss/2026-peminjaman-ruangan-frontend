896b1b024efce9671b8f32091cd2f40970d928a6
fathss <alwifathu@gmail.com>
2026-03-07 09:40:28 +0700

    fix: resolve lint/build warnings

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import AdminDashboard from "./pages/admin/AdminDashboard";
 import AddRoomPage from "./pages/admin/AddRoomPage";
 import EditRoomPage from "./pages/admin/EditRoomPage";
--- src/components/FormInput.tsx
+++ src/components/FormInput.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import type { FormInputProps } from "../types";
 
 function FormInput({ label, ...inputProps }: FormInputProps) {
