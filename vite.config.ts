85aa3b597a4cfd711fb503270651d463bb5fc9a2
fathss <alwifathu@gmail.com>
2026-04-06 19:51:15 +0700

    refactor: update nginx configuration and axios base URL for improved API handling

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,23 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(), 
-    tailwindcss()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [
+      react(), 
+      tailwindcss()],
+    server: {
+      port: 5173,
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL, 
+          changeOrigin: true,
+          secure: false,
+        }
+      }
+    }
+  }
 })
